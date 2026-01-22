bda1484861fbe4455cf341eae11b819caa2cfcdc
Slawosz Slawinski <slawosz@gmail.com>
2026-01-22 17:26:57 +0000

    Second commit - little bit navigation and forgoten files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,38 @@
 module ApplicationHelper
+  def main_menu_navigation_items
+
+    [
+      {
+        href: '/foo',
+        text: "Main data - hardcoded lists",
+        classes: "ml-auto",
+      },
+      {
+        href: '/foo',
+        text: "",
+        classes: "ml-auto",
+      },
+    ]
+  end
+
+  def sub_menu_navigation_items
+    [
+      {
+        header: "Important things",
+      },
+      {
+        legend: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur arcu erat, accumsan id imperdiet et, porttitor at semub_manu",
+      },
+      {
+        href: '/foo',
+        text: "Some section",
+        classes: "",
+      },
+      {
+        href: '/foo',
+        text: "Another section",
+        classes: "",
+      },
+    ]
+  end
 end

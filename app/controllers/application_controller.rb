7e52aa473b652955c9d848d6c667fef86cf4d8bd
Slawosz Slawinski <slawosz@gmail.com>
2026-07-03 09:19:44 +0100

    Add first guides

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_side_menu
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+  def set_side_menu
+    @side_menu = helpers.reference_data_sub_menu_navigation_items
+  end
 end
---
+++ app/controllers/guides_controller.rb
@@ -0,0 +1,23 @@
+class GuidesController < ApplicationController
+  GUIDES = %w[countries schools local_authorities passports initial_training_providers].freeze
+
+  def index
+    @guides = GUIDES
+  end
+
+  def show
+    @guide = params[:guide]
+
+    unless GUIDES.include?(@guide)
+      render plain: "Guide not found", status: :not_found
+      return
+    end
+
+    render @guide
+  end
+
+  private
+  def set_side_menu
+    @side_menu = helpers.guides_sub_menu_navigation_items
+  end
+end
